06c306c53a4e0e4b6cac243dd54762de045f5e22
hermenegildoAng <hernandezhermenegildoangeldavi@gmail.com>
2025-11-19 20:37:01 -0700

    intento 1 corregido

--- quickShort.java
+++ quickShort.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class quickShort {
 
-    
+    public static BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+    public static String entrada;
 
-    public static int[] prueba (int[] arreglo) throws IOException{
+    public static int[] prueba (int[] arreglo) {
         int aux;
-        int cambios;
-        cambios = 0;
+        int cambios = 0;
 
-        for (int i = 0; i < arreglo.length; i++) {
+        
+        for (int i = 0; i < arreglo.length - 1; i++) {
             if ( arreglo[i] > arreglo[i+1]) {
                 aux = arreglo[i];
                 arreglo[i] = arreglo[i+1];
@@ -19,51 +21,54 @@ public class quickShort {
                 cambios = cambios + 1;
             }
         }
-
         return arreglo;
     }
 
-    public static void imprimir(int[] arreglo) throws IOException{
+    public static void imprimir(int[] arreglo) {
+        
         for (int i = 0; i < arreglo.length; i++) {
             System.out.println(arreglo[i]);
         }
     }
 
     
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws IOException { 
         int x;
         x = 1;
 
-        int resp;
-
-        public static BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
-        public static String entrada;
+        int resp = 1;
 
         int[] arreglo = new int[5];
         
-
         arreglo[0] = 8;
         arreglo[1] = 4;
         arreglo[2] = 5;
         arreglo[3] = 6;
         arreglo[4] = 1;
 
-
-        while (x = 1) {
+        
+        while (resp == 1) {
+            
             arreglo = prueba(arreglo);
 
             System.out.print("Imprimir arreglo? si: (1) no: (0) ");
+
             entrada = bufer.readLine();
-            resp = Integer.parseInt(entrada);
-        
-            if (resp = 1) {
+            resp = Integer.parseInt(entrada.trim()); 
+            
+           
+            if (resp == 1) {
                 imprimir(arreglo);
             }
 
             System.out.print("Dar otra vuelta? si: (1) no: (0) ");
+         
             entrada = bufer.readLine();
-            resp = Integer.parseInt(entrada);
-
+            resp = Integer.parseInt(entrada.trim()); 
+            
         }
+        
+        System.out.println("Programa finalizado.");
     }
 }
