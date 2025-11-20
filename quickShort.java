4b18fcaf23ceccdf3255d982532824d8f78aaa24
hermenegildoAng <hernandezhermenegildoangeldavi@gmail.com>
2025-11-19 21:12:57 -0700

    quickShort particion

--- quickShort.java
+++ quickShort.java
@@ -7,68 +7,37 @@ public class quickShort {
 
     public static BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
     public static String entrada;
-
-    public static int[] prueba (int[] arreglo) {
-        int aux;
-        int cambios = 0;
-
-        
-        for (int i = 0; i < arreglo.length - 1; i++) {
-            if ( arreglo[i] > arreglo[i+1]) {
-                aux = arreglo[i];
-                arreglo[i] = arreglo[i+1];
-                arreglo[i+1] = aux;
-                cambios = cambios + 1;
-            }
-        }
-        return arreglo;
-    }
-
-    public static void imprimir(int[] arreglo) {
-        
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.println(arreglo[i]);
-        }
-    }
-
     
     
-    public static void main(String[] args) throws IOException { 
-        int x;
-        x = 1;
-
-        int resp = 1;
-
-        int[] arreglo = new int[5];
-        
-        arreglo[0] = 8;
-        arreglo[1] = 4;
-        arreglo[2] = 5;
-        arreglo[3] = 6;
-        arreglo[4] = 1;
-
-        
-        while (resp == 1) {
-            
-            arreglo = prueba(arreglo);
-
-            System.out.print("Imprimir arreglo? si: (1) no: (0) ");
-
-            entrada = bufer.readLine();
-            resp = Integer.parseInt(entrada.trim()); 
-            
-           
-            if (resp == 1) {
-                imprimir(arreglo);
-            }
-
-            System.out.print("Dar otra vuelta? si: (1) no: (0) ");
+    private static int particion(int[] arreglo, int bajo, int alto) {
          
-            entrada = bufer.readLine();
-            resp = Integer.parseInt(entrada.trim()); 
-            
-        }
+        int pivote = arreglo[alto]; 
         
-        System.out.println("Programa finalizado.");
+        int i = (bajo - 1);
+
+        for (int j = bajo; j < alto; j++) {
+            if (arreglo[j] <= pivote) {
+                i++;
+
+               
+                int aux = arreglo[i];
+                arreglo[i] = arreglo[j];
+                arreglo[j] = temp;
+            }
+        }
+
+        
+        int aux = arreglo[i + 1];
+        arreglo[i + 1] = arreglo[alto];
+        arreglo[alto] = temp;
+
+        return i + 1;
     }
+    
+   
+    
+    
+
+
+    
 }
